8854e488ec41cc5ae122a56342d0d0a7f52adbdc
Olek Nowicki <baraciak@gmail.com>
2019-01-17 11:47:37 +0100

    added modules folder

---
+++ modules/groceries.py
@@ -0,0 +1,18 @@
+import common
+import ui
+from os import sys
+
+GROCERIES_MENU = ('Generuj listę zakópów dla przepisu', 'Generuj listę zakupów dla jadłospisu')
+
+def start_module():
+    common.common_clear()
+    ui.print_menu('Menu zakupów:', GROCERIES_MENU, 'Wróc do menu głownego')
+    choose_option = ui.get_inputs(['Wybierz którąś z opcji:'], '')
+    if choose_option[0] == "1":
+        pass
+    elif choose_option[0] == "2":
+        pass
+    elif choose_option[0] == "0":
+        sys.exit(0)
+    else:
+        raise KeyError("There is no such option.")
---
+++ modules/week_menu.py
@@ -0,0 +1,25 @@
+import common
+import ui
+from os import sys
+
+WEEK_MENU = ('Wyświetl jadłospis', 'Dodaj jadłospis', 'Usuń jadłospis', 'Aktualizuj jadłospis*')
+
+def start_module():
+    common.common_clear()
+    ui.print_menu('Menu Jadłospisów:', WEEK_MENU, 'Wróc do menu głownego')
+    choose_option = ui.get_inputs(['Wybierz którąś z opcji:'], '')
+    if choose_option[0] == "1":
+        pass
+    elif choose_option[0] == "2":
+        pass
+    elif choose_option[0] == "3":
+        pass
+    elif choose_option[0] == "4":
+        pass
+    elif choose_option[0] == "0":
+        sys.exit(0)
+    else:
+        raise KeyError("There is no such option.")
+
+def random_menu():
+    pass
